27771e39e8a06431f01776542e01e3618ed63fb7
mattperkins <github@mattperkins.com>
2018-12-15 03:35:08 +0000

    correct schema types > uppercase

--- models/Notice.js
+++ models/Notice.js
@@ -1,13 +1,12 @@
 const Sq = require('sequelize')
-const db = require('./db')
+const db = require('../db')
 
 const Notice = db.define('notice', {
-  title: { type: Sq.String },
-  keywords: { type: Sq.String },
-  price: { type: Sq.String },
-  description: { type: Sq.String },
-  contact: { type: Sq.String },
-  image: { type: Sq.String }
+  title: { type: Sq.STRING },
+  keywords: { type: Sq.STRING },
+  price: { type: Sq.STRING },
+  description: { type: Sq.STRING },
+  contact: { type: Sq.STRING }
 })
 
 module.exports = Notice
