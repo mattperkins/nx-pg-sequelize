5109ba4dee974ea05164ebdef0dcd3a7b080522c
mattperkins <github@mattperkins.com>
2018-12-16 20:26:01 +0000

    configure search functionality

--- routes/notices.js
+++ routes/notices.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../db')
 const Notice = require('../models/Notice')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 // localhost:3006/notices > get Notices list
 router.get('/', (req, res) => Notice.findAll()
   .then(notices => {
-    console.log(notices)
+    // console.log(notices)
     // render view
     res.render('notices',
       {
@@ -45,6 +46,14 @@ router.post('/add', (req, res) => {
       errors, title, keywords, description, price, contact
     })
   } else {
+    if (!price) {
+      price = 'N/A'
+    } else {
+      price = `£${price}`
+    }
+    // convert comma space to , (globally)
+    keywords = keywords.toLowerCase().replace(/, /g, ',')
+
     // Insert form data into table
     Notice.create({
       title,
@@ -56,6 +65,20 @@ router.post('/add', (req, res) => {
       .then(notice => res.redirect('/notices'))
       .catch(err => console.log(err))
   }
+})
+
+// Search for Notices
+// this file puts us in the /notices/ route
+router.get('/search', (req, res) => {
+  // query = in url (address bar)
+  let { term } = req.query
+
+  // Force search query to lowercase
+  term = term.toLowerCase()
+
+  Notice.findAll({ where: { title: { [Op.like]: '%' + term + '%' } } })
+    .then(notices => res.render('notices', { notices }))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
